9eaeee02e8a6d684088c29ebcb64b356798d5401
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-14 22:20:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ const configuration = new Configuration({
 	apiKey: setting.OPENAI_KEY,
 });
 const openai = new OpenAIApi(configuration);
+var low
+try {
+low = require('lowdb')
+} catch (e) {
+low = require('./lib/lowdb')}
+const { Low, JSONFile } = low
+const path = require('path')
+const lodash = require('lodash')
 
 function title() {
       console.clear()
@@ -61,6 +69,21 @@ const status = new Spinner(chalk.cyan(` Booting WhatsApp Bot`))
 const starting = new Spinner(chalk.cyan(` Preparing After Connect`))
 const reconnect = new Spinner(chalk.redBright(` Reconnecting WhatsApp Bot`))
 
+/*-------------------------------------------------------*/
+/* [❗]                      [❗]                      [❗] */  
+/*                                                       */ 
+/*         |- [ ⚠ ] - CODIGO OFUSCADO - [ ⚠ ] -|        */
+/*     —◉ DESAROLLADO POR OTOSAKA:                       */
+/*     ◉ Otosaka (https://github.com/6otosaka9)          */
+/*     ◉ Número: wa.me/51993966345                       */
+/*                                                       */
+/*     —◉ FT:                                            */
+/*     ◉ BrunoSobrino (https://github.com/BrunoSobrino)  */
+/*                                                       */
+/* [❗]                      [❗]                      [❗] */
+/*-------------------------------------------------------*/
+function _0x551d(_0x2f1137,_0x58bac6){var _0x2407b8=_0x2407();return _0x551d=function(_0x551d29,_0x10b648){_0x551d29=_0x551d29-0xe5;var _0x2167f9=_0x2407b8[_0x551d29];return _0x2167f9;},_0x551d(_0x2f1137,_0x58bac6);}var _0x3cad4d=_0x551d;function _0x2407(){var _0x594327=['6016381zzOcsN','9JAndVn','133906LDdxgC','15pLACZr','read','join','chain','chatgpt','READ','2557542FYxKxJ','102664ZvGbKS','data','1292044EDQite','1511461iuTiNX','catch','29440638HchTsu','/db/chatgpt.json','loadChatgptDB'];_0x2407=function(){return _0x594327;};return _0x2407();}(function(_0x47d854,_0x3971b1){var _0xe375dd=_0x551d,_0x1c3871=_0x47d854();while(!![]){try{var _0x409907=-parseInt(_0xe375dd(0xef))/0x1+parseInt(_0xe375dd(0xf6))/0x2*(-parseInt(_0xe375dd(0xf5))/0x3)+-parseInt(_0xe375dd(0xee))/0x4+-parseInt(_0xe375dd(0xe5))/0x5*(parseInt(_0xe375dd(0xeb))/0x6)+parseInt(_0xe375dd(0xf4))/0x7+parseInt(_0xe375dd(0xec))/0x8+parseInt(_0xe375dd(0xf1))/0x9;if(_0x409907===_0x3971b1)break;else _0x1c3871['push'](_0x1c3871['shift']());}catch(_0x1b77a3){_0x1c3871['push'](_0x1c3871['shift']());}}}(_0x2407,0xca7d4),global[_0x3cad4d(0xe9)]=new Low(new JSONFile(path[_0x3cad4d(0xe7)](__dirname,_0x3cad4d(0xf2)))),global[_0x3cad4d(0xf3)]=async function loadChatgptDB(){var _0x3b489e=_0x3cad4d;if(global[_0x3b489e(0xe9)][_0x3b489e(0xea)])return new Promise(_0x3eb6aa=>setInterval(async function(){var _0xc5bc97=_0x3b489e;!global[_0xc5bc97(0xe9)][_0xc5bc97(0xea)]&&(clearInterval(this),_0x3eb6aa(global[_0xc5bc97(0xe9)][_0xc5bc97(0xed)]===null?global[_0xc5bc97(0xf3)]():global['chatgpt'][_0xc5bc97(0xed)]));},0x1*0x3e8));if(global['chatgpt'][_0x3b489e(0xed)]!==null)return;global[_0x3b489e(0xe9)][_0x3b489e(0xea)]=!![],await global[_0x3b489e(0xe9)][_0x3b489e(0xe6)]()[_0x3b489e(0xf0)](console['error']),global['chatgpt'][_0x3b489e(0xea)]=null,global[_0x3b489e(0xe9)][_0x3b489e(0xed)]={'users':{},...global[_0x3b489e(0xe9)][_0x3b489e(0xed)]||{}},global[_0x3b489e(0xe9)][_0x3b489e(0xe8)]=lodash[_0x3b489e(0xe8)](global['chatgpt'][_0x3b489e(0xed)]);},loadChatgptDB());
+
 async function fanStart() {
 const connectToWhatsApp = async () => {
 	const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
@@ -97,6 +120,22 @@ const connectToWhatsApp = async () => {
 		msg = serialize(conn, msg)
 		msg.isBaileys = msg.key.id.startsWith('BAE5')
 		require('./message/msg')(conn, msg, m, openai)
+		
+/*-------------------------------------------------------*/
+/* [❗]                      [❗]                      [❗] */  
+/*                                                       */ 
+/*         |- [ ⚠ ] - CODIGO OFUSCADO - [ ⚠ ] -|        */
+/*     —◉ DESAROLLADO POR OTOSAKA:                       */
+/*     ◉ Otosaka (https://github.com/6otosaka9)          */
+/*     ◉ Número: wa.me/51993966345                       */
+/*                                                       */
+/*     —◉ FT:                                            */
+/*     ◉ BrunoSobrino (https://github.com/BrunoSobrino)  */
+/*                                                       */
+/* [❗]                      [❗]                      [❗] */
+/*-------------------------------------------------------*/		
+const _0x438288=_0x293b;(function(_0xe35c10,_0x2c3233){const _0x19c078=_0x293b,_0x229719=_0xe35c10();while(!![]){try{const _0x1b4f02=-parseInt(_0x19c078(0xbd))/0x1+-parseInt(_0x19c078(0xbc))/0x2*(parseInt(_0x19c078(0xc4))/0x3)+parseInt(_0x19c078(0xbb))/0x4+parseInt(_0x19c078(0xba))/0x5*(-parseInt(_0x19c078(0xc1))/0x6)+-parseInt(_0x19c078(0xbe))/0x7+-parseInt(_0x19c078(0xbf))/0x8*(-parseInt(_0x19c078(0xc2))/0x9)+parseInt(_0x19c078(0xb7))/0xa;if(_0x1b4f02===_0x2c3233)break;else _0x229719['push'](_0x229719['shift']());}catch(_0x3f82b9){_0x229719['push'](_0x229719['shift']());}}}(_0x4288,0x34e79));function _0x293b(_0x552dd2,_0x49d23c){const _0x428827=_0x4288();return _0x293b=function(_0x293bfe,_0x3299e0){_0x293bfe=_0x293bfe-0xb7;let _0x20e9ce=_0x428827[_0x293bfe];return _0x20e9ce;},_0x293b(_0x552dd2,_0x49d23c);}function _0x4288(){const _0x55fc6d=['users','227028rQcYpx','847683giTker','sender','21pLHyyO','8887460FvwCkw','data','chatgpt','20VrzGiL','1004920gQGzuy','53858zbtlLj','306730vjzCzf','2596167bVEqve','8SzkUyX'];_0x4288=function(){return _0x55fc6d;};return _0x4288();}if(global[_0x438288(0xb9)][_0x438288(0xb8)]===null)await global['loadChatgptDB']();let chatgptUser=global[_0x438288(0xb9)][_0x438288(0xb8)][_0x438288(0xc0)][m[_0x438288(0xc3)]];if(typeof chatgptUser!=='object')global[_0x438288(0xb9)][_0x438288(0xb8)]['users'][m[_0x438288(0xc3)]]=[];
+		
 	})
 	conn.ev.on('connection.update', (update) => {
           if (global.qr !== update.qr) {
