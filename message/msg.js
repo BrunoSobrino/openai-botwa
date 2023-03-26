063f2cfb1fac1be7813fc590bfade2baa5f0f4f9
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-26 17:15:16 -0600

    Update msg.js

--- message/msg.js
+++ message/msg.js
@@ -54,12 +54,19 @@ module.exports = async (conn, msg, m, openai) => {
     const participants = msg.isGroup ? await groupMetadata.participants : ''
     const groupAdmins = msg.isGroup ? await getGroupAdmins(participants) : ''
     const isAdmin = msg.isGroup ? groupAdmins.includes(sender) : false
-    let senderJid;
-    if (msg.participant) {
-    senderJid = msg.participant;
-    } else {
-    senderJid = msg.sender;
-    }
+    
+let senderJid;
+if (msg.participant) {
+  senderJid = msg.participant;
+} else if (msg.isGroup && msg.key.remoteJid.endsWith('@g.us')) {
+  const participant = msg.participants.find((p) => p.jid === msg.key.remoteJid);
+  if (participant) {
+    senderJid = participant.jid;
+  }
+} else {
+  senderJid = msg.key.remoteJid;
+}
+
 
 
   
@@ -138,7 +145,7 @@ Comandos del Owner:
 conn.sendMessage(from, { text: textReply, mentions: [...textReply.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: msg });
 break
 case 'runtime':
-console.log(msg)    
+console.log(senderJid)    
 reply(require('../lib/myfunc').runtime(process.uptime()))
 break
 case 'ping':
